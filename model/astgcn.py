46c28c020816d58db3d528e772c84fa3b03e7766
Jilin Hu <hujilin1229@163.com>
2019-09-08 16:47:49 +0400

    multi gpus supports

--- model/astgcn.py
+++ model/astgcn.py
@@ -173,7 +173,12 @@ class Temporal_Attention_layer(nn.Block):
         for param in [self.U_1, self.U_2, self.U_3, self.b_e, self.V_e]:
             param._finish_deferred_init()
 
-        print(x)
+        print("=========================")
+        print("Context of Variables")
+        print("x::{0}, D1::{1}, D2::{2}".format(x.context, self.U_1.data().context, self.U_2.data().context))
+        print("=========================")
+
+        # print(x)
         # compute temporal attention scores
         # shape is (N, T, V)
         lhs = nd.dot(nd.dot(x.transpose((0, 3, 2, 1)), self.U_1.data()),
