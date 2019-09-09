cb165efce80d27d1f8a1573fb6a122e77b46e4df
Jilin Hu <hujilin1229@163.com>
2019-09-09 16:04:03 +0400

    fix the Parameter 'astgcn0_astgcn_block0_temporal_attention_layer0_U_1' was not initialized on context cpu(0) error

--- lib/utils.py
+++ lib/utils.py
@@ -338,6 +338,8 @@ def compute_val_loss_multigpu(net, val_loader, loss_function, sw, epoch, ctx):
         val_r = gluon.utils.split_and_load(val_r, ctx_list=ctx, even_split=False)
         val_t = gluon.utils.split_and_load(val_t, ctx_list=ctx, even_split=False)
 
+        print("val_w: ", val_w)
+
         outputs = [net([w, d, r]) for w, d, r in zip(val_w, val_d, val_r)]
         losses = [loss_function(o, l) for o, l in zip(outputs, val_t)]
 
@@ -345,6 +347,7 @@ def compute_val_loss_multigpu(net, val_loader, loss_function, sw, epoch, ctx):
         # tmp.extend(l.asnumpy().tolist())
         list_losses = []
         print("outputs: ", outputs[0])
+        print("label: ", val_t[0])
         print("num of losses is ", len(losses))
         for l in losses:
             print(l)
