a848389b2ec5779248f4e7a992dbc22f3405eb14
Jilin Hu <hujilin1229@163.com>
2019-09-09 16:47:22 +0400

    fix the Parameter 'astgcn0_astgcn_block0_temporal_attention_layer0_U_1' was not initialized on context cpu(0) error

--- train_Berlin_multigpu.py
+++ train_Berlin_multigpu.py
@@ -202,6 +202,7 @@ if __name__ == "__main__":
         val_d = gluon.utils.split_and_load(val_d, ctx_list=ctx, even_split=False)
         val_r = gluon.utils.split_and_load(val_r, ctx_list=ctx, even_split=False)
         outputs = [net([w, d, r]) for w, d, r in zip(val_w, val_d, val_r)]
+        print("test printing: ", outputs[0])
 
     net.initialize(ctx=ctx, init=MyInit(), force_reinit=True)
 
